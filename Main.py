9bf4f0a0e68d14a178e3951a1e55c07d5a160c18
Hadi Kiwan <hadi.k1.hk@gmail.com>
2020-06-19 14:26:57 +0200

    commit

--- Main.py
+++ Main.py
@@ -8,15 +8,17 @@ win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("First Game")
 
 # This goes outside the while loop, near the top of the program
-walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
-walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
-bg = pygame.image.load('bg.jpg')
-char = pygame.image.load('standing.png')
+walkRight = [pygame.image.load('Game Pics/R1.png'), pygame.image.load('Game Pics/R2.png'), pygame.image.load('Game Pics/R3.png'), pygame.image.load('Game Pics/R4.png'), pygame.image.load('Game Pics/R5.png'), pygame.image.load('Game Pics/R6.png'), pygame.image.load('Game Pics/R7.png'), pygame.image.load('Game Pics/R8.png'), pygame.image.load('Game Pics/R9.png')]
+walkLeft = [pygame.image.load('Game Pics/L1.png'), pygame.image.load('Game Pics/L2.png'), pygame.image.load('Game Pics/L3.png'), pygame.image.load('Game Pics/L4.png'), pygame.image.load('Game Pics/L5.png'), pygame.image.load('Game Pics/L6.png'), pygame.image.load('Game Pics/L7.png'), pygame.image.load('Game Pics/L8.png'), pygame.image.load('Game Pics/L9.png')]
+bg = pygame.image.load('Game Pics/bg.jpg')
+char = pygame.image.load('Game Pics/standing.png')
+
+clock = pygame.time.Clock()
 
 x = 50
 y =  425
-width = 40
-height = 60
+width = 64
+height = 64
 vel = 5
 left = False
 right = False
@@ -25,13 +27,31 @@ walkCount = 0
 isJump = False
 jumpCount = 10
 
+def redrawGameWindow():
+    global walkCount
+
+    win.blit(bg, (0, 0))
+
+    if walkCount + 1 >= 27:
+        walkCount = 0
+
+    if left:
+        win.blit(walkLeft[walkCount//3], (x, y))
+        walkCount += 1
+    elif right:
+        win.blit(walkRight[walkCount//3], (x, y))
+        walkCount += 1
+    else:
+        win.blit(char, (x, y))
+
+
+    pygame.display.update()
 
 
 #Main Loop
 run = True
 while run:
-    pygame.time.delay(100)
-
+    clock.tick(27)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -40,11 +60,22 @@ while run:
 
     if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT] and x < 500 - width - vel:
+        left = True
+        right = False
+    elif keys[pygame.K_RIGHT] and x < 500 - width - vel:
         x += vel
+        right = True
+        left = False
+    else:
+        right = False
+        left = False
+        walkCount = 0
     if not(isJump):
         if keys[pygame.K_SPACE]:
             isJump = True
+            right = False
+            left = False
+            walkCount = 0
     else:
         if jumpCount >= -10:
             neg = 1
@@ -55,10 +86,6 @@ while run:
         else:
             isJump = False
             jumpCount = 10
-
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
-    pygame.display.update()
-
+    redrawGameWindow()
 
 pygame.quit()
